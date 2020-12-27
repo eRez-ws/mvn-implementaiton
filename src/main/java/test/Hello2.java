be19658eccbf6a8b3e8c0f48505b21410a6dab43
eRez-ws <35656848+eRez-ws@users.noreply.github.com>
2020-12-27 11:23:38 +0200

    Update Hello2.java

--- src/main/java/test/Hello2.java
+++ src/main/java/test/Hello2.java
@@ -1,10 +1,10 @@
 package test;
 
-import io.netty.handler.ssl.OpenSslEngine;
+//import io.netty.handler.ssl.OpenSslEngine;
 
 public abstract class Hello2{
 
     public void eat(){
-        OpenSslEngine openSslEngine = new OpenSslEngine(2, null, null);
+        //OpenSslEngine openSslEngine = new OpenSslEngine(2, null, null);
     }
 }
